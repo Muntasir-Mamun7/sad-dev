85d48fe530c43a36e3681078786bf8ad035c416c
Muntasir Mamun <138435835+Muntasir-Mamun7@users.noreply.github.com>
2024-08-24 02:13:36 +0800

    Update new1.js
    drtg

--- new1.js
+++ new1.js
@@ -1,10 +1,13 @@
 //this is a comment
+let new_age = 78;
 let name = "Alice";
 let age = 25;
-let greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+let greeting = `Hello, my name is ${name} and I am ${age,new_age} years old.`;
 console.log(greeting);
 console.log("Nice to meet you!");
 console.log("I enjoy the following hobbies:");
 hobbies.forEach((hobby, index) => {
     console.log(`${index + 1}. ${hobby}`);
 });
+
+
