4c4b89225ff3b90b47dea012cc48b29e08cf7fcb
Muntasir Mamun <138435835+Muntasir-Mamun7@users.noreply.github.com>
2024-08-24 01:51:20 +0800

    Create new1.js

---
+++ new1.js
@@ -0,0 +1,6 @@
+//this is a comment
+let name = "Alice";
+let age = 25;
+let greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(greeting);
+console.log("Nice to meet you!");
